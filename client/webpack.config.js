a0df38ffacbb7eabe824b8094d9c824770044d35
mposk <mposk98@gmail.com>
2019-09-26 00:11:15 +0300

    added chat

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./newSrc/index.js",
+    entry: "./src/index.js",
     mode: "development",
     output: {
         filename: "./main.js",
